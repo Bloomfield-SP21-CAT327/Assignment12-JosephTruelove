07626fc37ad1f9a27ac5b2e534381187075e66c8
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2021-03-25 10:11:45 -0400

    Update 3 Setting up players
    Setting up players Complete, both host and client see player colors

--- Assignment12/Assets/Scripts/GameNetworkManager.cs
+++ Assignment12/Assets/Scripts/GameNetworkManager.cs
@@ -9,6 +9,7 @@ public class GameNetworkManager : NetworkManager
     {
         GameObject playerToSpawn = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
         playerToSpawn.GetComponent<Player>().color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        playerToSpawn.GetComponent<Renderer>().material.color = playerToSpawn.GetComponent<Player>().color;
         NetworkServer.AddPlayerForConnection(conn, playerToSpawn);
     }
 }
--- Assignment12/Assets/Scripts/Player.cs
+++ Assignment12/Assets/Scripts/Player.cs
@@ -7,5 +7,10 @@ public class Player : NetworkBehaviour
 {
     [SyncVar]
     public Color color;
-    
+
+    public override void OnStartClient()
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
 }

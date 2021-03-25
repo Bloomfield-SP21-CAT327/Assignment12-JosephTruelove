88c1f330f92ad47bbdd7d709c45f21c44fbb85d8
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2021-03-25 10:17:49 -0400

    Update 4
    Controlling a character Update complete. Player movement controls have been added

--- Assignment12/Assets/Scripts/Player.cs
+++ Assignment12/Assets/Scripts/Player.cs
@@ -7,6 +7,24 @@ public class Player : NetworkBehaviour
 {
     [SyncVar]
     public Color color;
+    public float moveSpeed = 1.875f;
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        MoveIt(x, y);
+    }
+
+    void MoveIt(float x, float y)
+    {
+        transform.Translate(x, y, 0);
+    }
 
     public override void OnStartClient()
     {

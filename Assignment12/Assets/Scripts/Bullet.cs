e501e8a64f332301201e2bd07e3cb3d570047a10
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2021-03-25 11:42:29 -0400

    Update 7
    Firing Weapons update complete - players can now fire bullets. ISSUE: only host can see bullets

---
+++ Assignment12/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Bullet : NetworkBehaviour
+{
+    [SyncVar]
+    public Color color;
+
+    public override void OnStartClient()
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+}
--- Assignment12/Assets/Scripts/Player.cs
+++ Assignment12/Assets/Scripts/Player.cs
@@ -8,10 +8,11 @@ public class Player : NetworkBehaviour
     [SyncVar]
     public Color color;
     public float moveSpeed = 1.875f;
+    public GameObject bulletPrefab;
 
     private void Update()
     {
-        if(isLocalPlayer)
+        if (isLocalPlayer)
         {
             GetInput();
         }
@@ -21,14 +22,19 @@ public class Player : NetworkBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
         float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
-        
-        if(isServer)
+
+        if (isServer)
         {
             RpcMoveIt(x, y);
         }
         else
         {
             CmdMoveIt(x, y);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            CmdDoFire();
         }
     }
 
@@ -44,9 +50,19 @@ public class Player : NetworkBehaviour
         RpcMoveIt(x, y);
     }
 
+    [Command]
+    public void CmdDoFire()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, this.transform.position + this.transform.right, Quaternion.identity);
+        bullet.GetComponent<Rigidbody>().velocity = Vector3.forward * 17.5f;
+        bullet.GetComponent<Bullet>().color = color;
+        Destroy(bullet, 0.875f);
+        NetworkServer.Spawn(bullet);
+    }
+
     public override void OnStartClient()
     {
         gameObject.GetComponent<Renderer>().material.color = color;
     }
-
 }
+    

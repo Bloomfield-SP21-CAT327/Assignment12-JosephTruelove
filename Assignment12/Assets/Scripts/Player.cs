92f473123dbf5037b3fa559ee6a6a21d7394eefd
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2021-03-25 10:30:09 -0400

    Update 5
    Self Control Update complete - Player can now only move on their own specific client

--- Assignment12/Assets/Scripts/Player.cs
+++ Assignment12/Assets/Scripts/Player.cs
@@ -11,7 +11,10 @@ public class Player : NetworkBehaviour
 
     private void Update()
     {
-        GetInput();
+        if(isLocalPlayer)
+        {
+            GetInput();
+        }
     }
 
     void GetInput()
